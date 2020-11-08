0217a48c4a8a070852f1236f664482247687c545
semyon-dev <semyon-dev@protonmail.com>
2020-11-08 14:46:57 +0300

    add backup

--- app/src/main/java/com/example/walletv1/SettingsActivity.kt
+++ app/src/main/java/com/example/walletv1/SettingsActivity.kt
@@ -3,9 +3,14 @@ package com.example.walletv1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.walletv1.utils.SecSharPref
+import java.io.File
+import java.io.FileInputStream
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +18,14 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
         title = "Settings"
 
-        val backupButton:Button = findViewById(R.id.backup)
+        val backupButton: Button = findViewById(R.id.backup)
         backupButton.setBackgroundColor(
             ContextCompat.getColor(
                 this,
                 R.color.colorButAndItemWalletAtiva
             )
         )
-        val deleteButton:Button = findViewById(R.id.delete_wallet)
+        val deleteButton: Button = findViewById(R.id.delete_wallet)
         deleteButton.setBackgroundColor(
             ContextCompat.getColor(
                 this,
@@ -35,6 +40,16 @@ class SettingsActivity : AppCompatActivity() {
             intent.flags =
                 Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+        }
+
+        backupButton.setOnClickListener {
+            // val path = this.getExternalFilesDir(null)
+            //Log.e("TAG --------------", path.toString())
+            val letDirectory = File("/storage/emulated/0", "BGL_Backup")
+            letDirectory.mkdirs()
+            //val file = File(letDirectory, "24words_backup.txt")
+            // file.appendText("record goes here bla bla bla")
+            Toast.makeText(this, "Backup is success", Toast.LENGTH_SHORT).show()
         }
     }
 }
