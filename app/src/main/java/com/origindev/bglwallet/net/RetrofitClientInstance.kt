f52f07b942570485c679964550d2a854bbff6152
GranPecador <lennuz17@gmail.com>
2020-12-04 04:07:05 +0300

    delete interceptor

--- app/src/main/java/com/origindev/bglwallet/net/RetrofitClientInstance.kt
+++ app/src/main/java/com/origindev/bglwallet/net/RetrofitClientInstance.kt
@@ -2,7 +2,6 @@ package com.origindev.bglwallet.net
 
 import com.origindev.bglwallet.BuildConfig
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,14 +9,10 @@ object RetrofitClientInstance {
 
     private const val BASE_URL = BuildConfig.SERVER_URL
 
-    private val logging = HttpLoggingInterceptor()
 
     private val client = OkHttpClient.Builder()
 
     val instance: JSONPlaceHolderApi by lazy {
-
-        logging.level = HttpLoggingInterceptor.Level.BODY
-        client.addInterceptor(logging)
 
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
--- app/src/main/java/com/origindev/bglwallet/net/UsdRetrofitClientInstance.kt
+++ app/src/main/java/com/origindev/bglwallet/net/UsdRetrofitClientInstance.kt
@@ -1,7 +1,6 @@
 package com.origindev.bglwallet.net
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,14 +9,9 @@ import retrofit2.http.GET
 object UsdRetrofitClientInstance {
     private const val BASE_URL = "https://api.coingecko.com"//?ids=bitgesell&vs_currencies=usd
 
-    private val logging = HttpLoggingInterceptor()
-
     private val client = OkHttpClient.Builder()
 
     val instance: UsdJsonPlaceHolderApi by lazy {
-
-        logging.level = HttpLoggingInterceptor.Level.BODY
-        client.addInterceptor(logging)
 
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
